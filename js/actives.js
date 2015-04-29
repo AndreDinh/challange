01b513da73193eb7763b281090a68e3e4a884342
Andre Dinh <dinh.andre@yahoo.com>
2015-04-29 16:13:43 -0700

    change strike to css and made it to toggle class

--- js/actives.js
+++ js/actives.js
@@ -1,6 +1,19 @@
 $(function() {
     $('input:checkbox').on('change', function() {
-        $(this).parent("li").wrap("<s>");
+        $(this).parent("li").toggleClass('stroked');
+        // $(this).parent("li").unwrap("<s>");
     });
 });
 
+// var strike = $( "s" );
+// $( "input:checkbox" ).on('change', function(){
+//   if ( strike.parent("li").is("s")) {
+//     strike.unwrap();
+//   } else {
+//     strike.wrap( "<s>" );
+//   }
+// });
+
+// find s then unwrap or wrap input
+
+// if parent("li").find("s")
